0bc2e8357bc09a53827ab62c1a6c2b733b688c72
Rahul Gupta <rg5353070@gmail.com>
2024-02-15 18:35:53 +0530

    removed the best seller slice

--- slices/BestSellerSection/index.tsx
+++ slices/BestSellerSection/index.tsx
@@ -7,24 +7,12 @@ export type BestSellerSectionProps =
   SliceComponentProps<Content.BestSellerSectionSlice>;
 
 const BestSellerSection = async ({ slice }: BestSellerSectionProps) => {
-  const client = createClient();
-  const featuredProducts = await client.getAllByType("featured_products");
-
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="big-container block-space grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4"
-    >
-      {featuredProducts.map((product, index) => (
-        <FeaturedProductCard
-          key={index}
-          productImage={product.data.featured_product_main_image}
-          productName={product.data.product_name}
-          productPrice={product.data.product_price}
-        />
-      ))}
-    </section>
+    ></section>
   );
 };
 
